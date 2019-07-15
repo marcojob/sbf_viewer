6afa45b7b35c2750c2bf85b7fa045e541f993a83
marcojob <marco.job@bluewin.ch>
2019-07-15 13:55:47 +0200

    added correct satellite naming

--- src/sbf_satellite.py
+++ src/sbf_satellite.py
@@ -31,29 +31,33 @@ class Satellite:
 
     def to_dict_df(self):
         for num in self.signals:
+            print(self.sig_num_ref[num])
             if self.sig_num_ref[num]['band'] == 1:
                 for sat in self.signals[num]:
+                    print(sat)
                     self.dict_df_1[sat] = pd.DataFrame(data=self.signals[num][sat]['snr'], 
                                                   index=self.signals[num][sat]['tow'])
             elif self.sig_num_ref[num]['band'] == 2:
                 for sat in self.signals[num]:
+                    print(sat)
                     self.dict_df_2[sat] = pd.DataFrame(data=self.signals[num][sat]['snr'],
                                                   index=self.signals[num][sat]['tow'])
 
     def update_signals(self, tow, wnc, svid, sig_type, cn0, locktime):
         sig_num = self.get_band(sig_type)
+        svid_offset = self.get_svid(svid)
         snr = self.get_snr(cn0, sig_num)
         if not sig_num in self.signals.keys():
             self.signals[sig_num] = {}
 
-        if not svid in self.signals[sig_num].keys():
-            self.signals[sig_num][svid] = {
+        if not svid_offset in self.signals[sig_num].keys():
+            self.signals[sig_num][svid_offset] = {
                 'snr': [],
                 'tow': []
             }
 
-        self.signals[sig_num][svid]['snr'].append(snr)
-        self.signals[sig_num][svid]['tow'].append(tow)
+        self.signals[sig_num][svid_offset]['snr'].append(snr)
+        self.signals[sig_num][svid_offset]['tow'].append(tow)
 
     def update_events(self, tow, wnc):
         self.events['tow'].append(tow)
@@ -63,3 +67,25 @@ class Satellite:
 
     def get_snr(self, cn0, sig_num):
         return cn0*0.25 if sig_num == 1 or sig_num == 2 else cn0*0.25+10.0
+
+    def get_svid(self, svid):
+        '''Table 4.1.9'''
+        if svid >= 1 and svid <= 37:
+            return 'G{:02d}'.format(svid)
+        elif svid >= 38 and svid <= 61:
+            return 'R{:02d}'.format(svid-37)
+        elif svid >= 63 and svid <= 68:
+            return 'R{:02d}'.format(svid-38)
+        elif svid >= 71 and svid <= 106:
+            return 'E{:02d}'.format(svid-70)
+        elif svid >= 120 and svid <= 140:
+            return 'S{:02d}'.format(svid-100)
+        elif svid >= 141 and svid <= 177:
+            return 'C{:02d}'.format(svid-40)
+        elif svid >= 181 and svid <= 187:
+            return 'J{:02d}'.format(svid-180)
+        elif svid >= 191 and svid <= 197:
+            return 'I{:02d}'.format(svid-190)
+        elif svid >= 198 and svid <= 215:
+            return 'S{:02d}'.format(svid-157)
+        return svid
